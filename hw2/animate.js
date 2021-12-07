f2772f90ef6e692a7f1a1a251dd4151d94b3ec6e
FLYYU5683 <woody90514@gmail.com>
2021-12-07 20:27:21 +0800

    error

--- hw2/animate.js
+++ hw2/animate.js
@@ -1,10 +1,11 @@
-import {camera,group,Last_P,clock} from "./init.js";
+import {camera,group,Last_P,Last_Q,clock} from "./init.js";
 import {check_Collision,IsBCollision,IsCollision} from "./check_Collision.js";
-import {update,pos,angle,reSetPower} from "./update.js";
+import {update,pos,angle,reSetPower,setCol} from "./update.js";
 import {render} from "./render.js";
 import {makeRect} from "./makeRect.js";
 import * as THREE from "https://threejs.org/build/three.module.js";
 var backOnce = false;
+var temp;
 (function() {
   Math.clamp = function(val,min,max){
     return Math.min(Math.max(val,min),max);
@@ -18,23 +19,30 @@ function animate() {
   camera.position.copy(group.localToWorld(new THREE.Vector3(-60,20,5)));
   camera.lookAt(group.localToWorld(new THREE.Vector3(10,0,5)));
 
+  //console.log(temp,angle);
+	console.log(`現在的(${group.quaternion.x},${group.quaternion.y},${group.quaternion.z})`);
+	console.log(`上一幀(${Last_Q.x},${Last_Q.y},${Last_Q.z})`);
   if(IsCollision || IsBCollision){
+	console.log("撞到了-------------------------------------------");
     reSetPower();
-	console.log("boom");
-	if(!backOnce){
+	/*if(!backOnce){
 	  backOnce = true; 
-	  pos.copy(Last_P);
 	}
-    group.position.copy(pos);
-	//group.quaternion.copy(Last_Q);
-    group.rotation.y = angle;
+	*/
+	  pos.copy(Last_P);
+      group.position.copy(pos);
+	  group.quaternion.copy(Last_Q);
+	  //setCol();
+      group.rotation.y = angle;
   }
   else{
+	console.log("還沒有撞到-------------------------------------------");
     backOnce = false;
     Last_P.copy(group.position);
-	//Last_Q.copy(group.quaternion);
+	Last_Q.copy(group.quaternion);
 	group.position.copy(pos);
-    group.rotation.y = angle;	  	
+    group.rotation.y = angle;
+    //temp = angle;	
   }
   
   var dt = clock.getDelta();
@@ -43,4 +51,4 @@ function animate() {
   requestAnimationFrame(animate);
   render();
 }
-export{animate};
+export{animate,temp};
