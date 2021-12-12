110dbe857406a9fbc1a33136ce3a2733ac2cc616
FLYYU5683 <woody90514@gmail.com>
2021-12-12 11:08:49 +0800

    fin

--- hw4/method.js
+++ hw4/method.js
@@ -23,39 +23,53 @@ function doPointerDown (event) {
 	  if(intersects[0].object.name == "fire"){
 	    switch(intersects[0].object.parent.name){
 		  case 0:
-		    intersects[0].object.parent.children[1].visible = false;
-		    intersects[0].object.parent.children[2].visible = false;
-		    candles[0].toReStartAgain();
+			if(intersects[0].object.parent.children[1].visible){
+		      intersects[0].object.parent.children[1].visible = false;
+		      intersects[0].object.parent.children[2].visible = false;
+			  candles[0].toReStartAgain();
+			}
 			break;		  
 		  case 1:
-		    intersects[0].object.parent.children[1].visible = false;
-		    intersects[0].object.parent.children[2].visible = false;
-		    candles[1].toReStartAgain();
+		    if(intersects[0].object.parent.children[1].visible){
+		      intersects[0].object.parent.children[1].visible = false;
+		      intersects[0].object.parent.children[2].visible = false;
+		      candles[1].toReStartAgain();
+			}
 			break;		  
 		  case 2:
-		    intersects[0].object.parent.children[1].visible = false;
-		    intersects[0].object.parent.children[2].visible = false;
-		    candles[2].toReStartAgain();
+		    if(intersects[0].object.parent.children[1].visible){
+		      intersects[0].object.parent.children[1].visible = false;
+		      intersects[0].object.parent.children[2].visible = false;
+		      candles[2].toReStartAgain();
+			}
 			break;		  
 	      case 3:
-		    intersects[0].object.parent.children[1].visible = false;
-		    intersects[0].object.parent.children[2].visible = false;
-		    candles[3].toReStartAgain();
+		    if(intersects[0].object.parent.children[1].visible){
+		      intersects[0].object.parent.children[1].visible = false;
+		      intersects[0].object.parent.children[2].visible = false;
+		      candles[3].toReStartAgain();
+			}
 			break;		  
 	      case 4:
-		    intersects[0].object.parent.children[1].visible = false;
-		    intersects[0].object.parent.children[2].visible = false;
-		    candles[4].toReStartAgain();
+		    if(intersects[0].object.parent.children[1].visible){
+		      intersects[0].object.parent.children[1].visible = false;
+		      intersects[0].object.parent.children[2].visible = false;
+		      candles[4].toReStartAgain();
+			}
 			break;		  
 	      case 5:
-		    intersects[0].object.parent.children[1].visible = false;
-		    intersects[0].object.parent.children[2].visible = false;
-		    candles[5].toReStartAgain();
+		    if(intersects[0].object.parent.children[1].visible){
+		      intersects[0].object.parent.children[1].visible = false;
+		      intersects[0].object.parent.children[2].visible = false;
+		      candles[5].toReStartAgain();
+            }
 			break;		  
 	      case 6:
-		    intersects[0].object.parent.children[1].visible = false;
-		    intersects[0].object.parent.children[2].visible = false;
-		    candles[6].toReStartAgain();
+		    if(intersects[0].object.parent.children[1].visible){
+		      intersects[0].object.parent.children[1].visible = false;
+		      intersects[0].object.parent.children[2].visible = false;
+		      candles[6].toReStartAgain();
+			}
 			break;
 		}		
 	  }
