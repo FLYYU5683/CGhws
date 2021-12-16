229937d68a750cff61090844de140d2cefea46c1
FLYYU5683 <woody90514@gmail.com>
2021-12-16 20:15:44 +0800

    fin

--- 2.js
+++
@@ -1,8 +0,0 @@
-import {camera,renderer} from "./hw2.html";
-
-function onWindowResize() {
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
-  renderer.setSize(window.innerWidth, window.innerHeight);
-}
-export {onWindowResize};
